8cabac88721377fb2e8846339a7219285f99b7e3
FelipeDuMato <felipenomer18@gmail.com>
2025-09-19 08:00:16 -0300

    Mudando scripts sql

--- felipe galante/Scripts SQL Criar Tabela Doacoes.sql
+++ felipe galante/Scripts SQL Criar Tabela Doacoes.sql
@@ -3,9 +3,11 @@ id INT PRIMARY KEY auto_increment,
 data DATE NOT NULL,
 tipo CHAR(1) NOT NULL, -- "D" Dinheiro, "A" Alimento , "O" Outros
 obs CHAR(150),
-doador INT,
+doador INT NOT NULL,
 idoso INT,
-evento INT
+evento INT,
+CONSTRAINT fk_doacaoDoador
+FOREIGN KEY (doador) REFERENCES doadores(id)
 );
 
 CREATE TABLE doacaoDinheiro (
